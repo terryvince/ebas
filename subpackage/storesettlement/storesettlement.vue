db6a6a3b11726bd5aa44f48ed13fdbcad48ba4c5
Administrator <1606984381@qq.com>
2020-11-23 18:46:08 +0800

    1

--- subpackage/storesettlement/storesettlement.vue
+++ subpackage/storesettlement/storesettlement.vue
@@ -430,7 +430,7 @@
 					postSettlement(this.storeInfo).then(res=>{
 						if(res.data=="成功"){
 							uni.showToast({
-							  title: "入驻成功",
+							  title: "申请成功",
 							  icon: "none",
 							  duration: 2000
 							});
