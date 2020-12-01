11561f23e4d3e3db976b06a7427796f9ebee3c7a
Bruce <c6357@outlook.com>
2020-12-01 11:21:26 +0800

    1

--- pages/user/coupon/GetCoupon/index.vue
+++ pages/user/coupon/GetCoupon/index.vue
@@ -1,107 +1,160 @@
-<template>
-  <view ref="container">
-    <div class="coupon-list" v-if="couponsList.length > 0">
-      <div
-        class="item acea-row row-center-wrapper"
-        v-for="(item, index) in couponsList"
-        :key="index"
-      >
-        <div class="money" :class="item.isUse ? 'moneyGray' : ''">
-          <div>
-            ￥
-            <span class="num">{{ item.couponPrice }}</span>
-          </div>
-          <div class="pic-num">满{{ item.useMinPrice }}元可用</div>
-        </div>
-        <div class="text">
-          <div class="condition line1">
-            <span class="line-title bg-color-check" v-if="item.ctype === 0">通用劵</span>
-            <span class="line-title bg-color-check" v-else-if="item.ctype === 1">商品券</span>
-            <span class="line-title bg-color-check" v-else>未知</span>
-            <span>{{ item.cname }}</span>
-          </div>
-          <div class="data acea-row row-between-wrapper">
-            <div v-if="item.endTime !== 0">{{ item.startTime }}-{{ item.endTime }}</div>
-            <div v-else>不限时</div>
-            <div class="bnt gray" v-if="item.isUse === true">已领取</div>
-            <div class="bnt gray" v-else-if="item.isUse === 2">已领完</div>
-            <div class="bnt bg-color-red" v-else @click="getCoupon(item.id, index)">立即领取</div>
-          </div>
-        </div>
-      </div>
-    </div>
+<style lang="less">
+	.coupon-list .item {
+		width: 100%;
+		height: unset !important;
+		// min-height: 2.2*100rpx;
+		margin-bottom: 0.16*100rpx;
+		background-color: #FFFFFF;
+	}
 
-    <Loading :loaded="loadend" :loading="loading"></Loading>
-    <!--暂无优惠券-->
-    <view class="noCommodity" v-if="couponsList.length === 0 && page > 1">
-      <view class="noPictrue">
-        <image src="@/static/images/noCoupon.png" class="image" />
-      </view>
-    </view>
-  </view>
+	.coupon-list .item .text {
+		// margin-top: 0rpx;
+	}
+
+	.coupon-list .item .money {
+		height: unset !important;
+	}
+
+	.explian {
+		display: flex;
+		justify-content: space-between;
+		height: 40rpx;
+		overflow: hidden;
+		padding: 20rpx 0rpx;
+		// padding-top: 20rpx;
+		// margin-bottom: 40rpx;
+		background-color: #FFFFFF;
+		// margin-top: 20rpx;
+		// margin-bottom: 20rpx;
+	}
+</style>
+
+<template>
+	<view ref="container">
+		<!-- 		<view class="flex-main-between" style="height: 60rpx; font-size: 30rpx; padding-left: 30rpx;padding-right: 30rpx;">
+			<view style="margin-left:0rpx; line-height: 60rpx; margin-top: 20rpx;">
+				可领取的优惠券
+			</view>
+			<view @click="couponAction" style="margin-right:0rpx; line-height: 60rpx; margin-top: 20rpx;color: #999999;">
+				使用说明>
+			</view>
+		</view> -->
+		<div class="coupon-list" v-if="couponsList.length > 0">
+			<div class="item flex-column" v-for="(item, index) in couponsList" :key="index">
+				<view class="acea-row">
+					<div class="money" :class="item.isUse ? 'moneyGray' : ''">
+						<div>
+							￥
+							<span class="num">{{ item.couponPrice }}</span>
+						</div>
+						<div class="pic-num">满{{ item.useMinPrice }}元可用</div>
+					</div>
+					<div class="text">
+						<div class="condition line1">
+							<span class="line-title bg-color-check" v-if="item.ctype === 0">通用劵</span>
+							<span class="line-title bg-color-check" v-else-if="item.ctype === 1">商品券</span>
+							<span class="line-title bg-color-check" v-else>未知</span>
+							<span>{{ item.cname }}</span>
+						</div>
+						<div class="data acea-row row-between-wrapper">
+							<div v-if="item.endTime !== 0">{{ item.startTime }}-{{ item.endTime }}</div>
+							<div v-else>不限时</div>
+							<div class="bnt gray" v-if="item.isUse === true">已领取</div>
+							<div class="bnt gray" v-else-if="item.isUse === 2">已领完</div>
+							<div class="bnt bg-color-red" v-else @click="getCoupon(item.id, index)">立即领取</div>
+						</div>
+					</div>
+				</view>
+				<view class="explian">
+					<view style=" font-size: 30rpx; color: #999999;">使用说明使用说明使用说明使用说明使用说明使用说明使用说明使用说明？</view>
+					<view>v</view>
+				</view>
+			</div>
+
+
+		</div>
+
+		<Loading :loaded="loadend" :loading="loading"></Loading>
+		<!--暂无优惠券-->
+		<view class="noCommodity" v-if="couponsList.length === 0 && page > 1">
+			<view class="noPictrue">
+				<image src="@/static/images/noCoupon.png" class="image" />
+			</view>
+		</view>
+	</view>
 </template>
 <script>
-import { getCoupon, getCouponReceive } from "@/api/user";
-import Loading from "@/components/Loading";
-import DataFormatT from "@/components/DataFormatT";
-export default {
-  name: "getCoupon",
-  components: {
-    Loading,
-    DataFormatT
-  },
-  props: {},
-  data: function() {
-    return {
-      page: 1,
-      limit: 10,
-      couponsList: [],
-      loading: false,
-      loadend: false
-    };
-  },
-  mounted: function() {
-    this.getUseCoupons();
-  },
-  onReachBottom() {
-    !this.loading && this.getUseCoupons();
-  },
-  methods: {
-    getCoupon: function(id, index) {
-      let that = this;
-      let list = that.couponsList;
-      getCouponReceive(id)
-        .then(function(res) {
-          list[index].isUse = true;
-          uni.showToast({
-            title: "领取成功",
-            icon: "success",
-            duration: 2000
-          });
-        })
-        .catch(function(err) {
-          uni.showToast({
-            title:
-              err.msg || err.response.data.msg || err.response.data.message,
-            icon: "none",
-            duration: 2000
-          });
-        });
-    },
-    getUseCoupons: function() {
-      let that = this;
-      if (that.loading) return; //阻止下次请求（false可以进行请求）；
-      if (that.loadend) return; //阻止结束当前请求（false可以进行请求）；
-      that.loading = true;
-      let q = { page: that.page, limit: that.limit,merId:that.$yroute.query.merId||''};
-      getCoupon(q).then(res => {
-        that.loading = false;
-        //apply();js将一个数组插入另一个数组;
-        that.couponsList.push.apply(that.couponsList, res.data);
-        that.loadend = res.data.length < that.limit; //判断所有数据是否加载完成；
-        that.page = that.page + 1;
-      });
-    }
-  }
-};
+	import {
+		getCoupon,
+		getCouponReceive
+	} from "@/api/user";
+	import Loading from "@/components/Loading";
+	import DataFormatT from "@/components/DataFormatT";
+	export default {
+		name: "getCoupon",
+		components: {
+			Loading,
+			DataFormatT
+		},
+		props: {},
+		data: function() {
+			return {
+				page: 1,
+				limit: 10,
+				couponsList: [],
+				loading: false,
+				loadend: false
+			};
+		},
+		mounted: function() {
+			this.getUseCoupons();
+		},
+		onReachBottom() {
+			!this.loading && this.getUseCoupons();
+		},
+		methods: {
+			couponAction: function() {
+				console.log("couponAction");
+				this.$yrouter.push("/subpackage/couponProtocol/couponProtocol");
+			},
+			getCoupon: function(id, index) {
+				let that = this;
+				let list = that.couponsList;
+				getCouponReceive(id)
+					.then(function(res) {
+						list[index].isUse = true;
+						uni.showToast({
+							title: "领取成功",
+							icon: "success",
+							duration: 2000
+						});
+					})
+					.catch(function(err) {
+						uni.showToast({
+							title: err.msg || err.response.data.msg || err.response.data.message,
+							icon: "none",
+							duration: 2000
+						});
+					});
+			},
+			getUseCoupons: function() {
+				let that = this;
+				if (that.loading) return; //阻止下次请求（false可以进行请求）；
+				if (that.loadend) return; //阻止结束当前请求（false可以进行请求）；
+				that.loading = true;
+				let q = {
+					page: that.page,
+					limit: that.limit,
+					merId: that.$yroute.query.merId || ''
+				};
+				getCoupon(q).then(res => {
+					that.loading = false;
+					//apply();js将一个数组插入另一个数组;
+					that.couponsList.push.apply(that.couponsList, res.data);
+					that.loadend = res.data.length < that.limit; //判断所有数据是否加载完成；
+					that.page = that.page + 1;
+				});
+			}
+		}
+	};
 </script>
